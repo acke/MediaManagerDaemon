39a5b4ebb4f1fd3a7fed92ccd827d7b6154f6d89
acke <knut.funkel@gmail.com>
2009-07-23 17:03:39 +0200

    rewritten, but more work needs to be done.

--- MediaManagerDaemon/INIFileReader.cs
+++ MediaManagerDaemon/INIFileReader.cs
@@ -1,64 +1,65 @@
 //Outch.. got some code pasted in this file. WHATAMESS....
 
+using System.IO;
+using System;
 
 namespace MediaManagerDaemon
 {
     class INIFileReader
     {
-        var data = new INIFileReader<string, string>();
-        foreach (var row in File.ReadAllLines(PATH_TO_FILE))
-          data.Add(row.Split('=')[0], row.Split('=')[1]);
+             
+        public static Dictionary<string, string> GetProperties(string path)
+        {
+            string fileData = "";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                fileData = sr.ReadToEnd().Replace("\r", "");
+            }
+            Dictionary<string, string> Properties = new Dictionary<string, string>();
+            string[] kvp;
+            string[] records = fileData.Split("\n".ToCharArray());
+            foreach (string record in records)
+            {
+                kvp = record.Split("=".ToCharArray());
+                Properties.Add(kvp[0], kvp[1]);
+            }
+            return Properties;
+        }
 
-        //Console.WriteLine(data["ServerName"]);
-    }
+//Here's an example of how to use it:
 
-
-//INIFileReader<string,string> Properties = GetProperties("data.txt");
+Dictionary<string,string> Properties = GetProperties("data.txt");
 //Console.WriteLine("Hello: " + Properties["Hello"]);
 //Console.ReadKey();
 
-public static INIFileReader<string, string> GetProperties(string path)
-{
-    string fileData = "";
-    using (StreamReader sr = new StreamReader(path))
-    {
-        fileData = sr.ReadToEnd().Replace("\r", "");
-    }
-    INIFileReader<string, string> Properties = new INIFileReader<string, string>();
-    string[] kvp;
-    string[] records = fileData.Split("\n".ToCharArray());
-    foreach (string record in records)
-    {
-        kvp = record.Split("=".ToCharArray());
-        Properties.Add(kvp[0], kvp[1]);
-    }
-    return Properties;
-}
 
-    public static IINIFileReader ParseINIFileReader(string fileName)
-    {
-        INIFileReader<string, string> iniFileProperties = new INIFileReader<string, string>();
-        foreach (string line in File.ReadAllLines(fileName))
+
+        public static IINIFileReader ParseINIFileReader(string fileName)
         {
-            if ((!string.IsNullOrEmpty(line)) &&
-                (!line.StartsWith(";")) &&
-                (!line.StartsWith("#")) &&
-                (!line.StartsWith("'")) &&
-                (line.Contains('=')))
+            INIFileReader<string, string> iniFileProperties = new INIFileReader<string, string>();
+            foreach (string line in File.ReadAllLines(fileName))
             {
-                int index = line.IndexOf('=');
-                string key = line.Substring(0, index).Trim();
-                string value = line.Substring(index + 1).Trim();
-
-                if ((value.StartsWith("\"") && value.EndsWith("\"")) ||
-                    (value.StartsWith("'") && value.EndsWith("'")))
+                if ((!string.IsNullOrEmpty(line)) &&
+                    (!line.StartsWith(";")) &&
+                    (!line.StartsWith("#")) &&
+                    (!line.StartsWith("'")) &&
+                    (line.Contains('=')))
                 {
-                    value = value.Substring(1, value.Length - 2);
+                    int index = line.IndexOf('=');
+                    string key = line.Substring(0, index).Trim();
+                    string value = line.Substring(index + 1).Trim();
+
+                    if ((value.StartsWith("\"") && value.EndsWith("\"")) ||
+                        (value.StartsWith("'") && value.EndsWith("'")))
+                    {
+                        value = value.Substring(1, value.Length - 2);
+                    }
+                    iniFileProperties.Add(key, value);
                 }
-                iniFileProperties.Add(key, value);
             }
+
+            return iniFileProperties;
         }
 
-        return iniFileProperties;
-    }
+   }
 }
